2c3afd36cdc35cfc4f59645bdfdff97bd818082f
Pinsky79 <pfmarinsky@gmail.com>
2018-12-14 01:28:25 -0600

    updated jquery

--- JS/main.js
+++ JS/main.js
@@ -1,25 +1,28 @@
-//On Click Open Contact Block
-$('.contact-block, .menu-item.contact').click(function () {
-    $('.content-blocks').removeClass('show');
-    $('.content-blocks.contact').addClass('show');
-    $('.menu-item').removeClass('active');
-    $('.menu-item.contact').addClass('active');
-});
+$(document).ready(function () {
 
-//On Click Open About/Resume Block
-$('.about-block, .menu-item.about').click(function () {
-    $('.content-blocks').removeClass('show');
-    $('.content-blocks.about').addClass('show');
-    $('.menu-item').removeClass('active');
-    $('.menu-item.about').addClass('active');
+    //On Click Open Contact Block
+    $('.contact-block, .menu-item.contact').click(function () {
+        $('.content-blocks').removeClass('show');
+        $('.content-blocks.contact').addClass('show');
+        $('.menu-item').removeClass('active');
+        $('.menu-item.contact').addClass('active');
+    });
+
+    //On Click Open About/Resume Block
+    $('.about-block, .menu-item.about').click(function () {
+        $('.content-blocks').removeClass('show');
+        $('.content-blocks.about').addClass('show');
+        $('.menu-item').removeClass('active');
+        $('.menu-item.about').addClass('active');
+    });
 });
 
 /*
 (function ($) {
     "use strict";
-
+    
     $('.portfolio-block, .menu-item').on('click', function () {
-
+    
         //Portfolio masonry
         var $container = $('#portfolio-container');
         $container.isotope({
@@ -37,7 +40,7 @@ $('.about-block, .menu-item.about').click(function () {
             });
         });
     });
-
+    
     //On Click Open Menu Items
 $('.menu-block, .menu-item').click(function () {
     $('.name-block').addClass('reverse');
@@ -45,7 +48,7 @@ $('.menu-block, .menu-item').click(function () {
     $('.menu-blocks').addClass('hide');
     $('.inline-menu-container').addClass('show');
 });
-
+    
     //Portfolio Modal
     $(document).on('click', '.open-project', function () {
         var projectUrl = $(this).attr("href");
@@ -55,7 +58,7 @@ $('.menu-block, .menu-item').click(function () {
         $('.content-blocks.pop section').load(projectUrl + ' .load-data > *');
         return false;
     });
-
+    
     //Blog post Modal
     $('.open-post').on('click', function () {
         var postUrl = $(this).attr("href");
@@ -65,8 +68,8 @@ $('.menu-block, .menu-item').click(function () {
         $('.content-blocks.pop section').load(postUrl);
         return false;
     });
-
-
+    
+    
    
     //On Click Open Portfolio Block
     $('.portfolio-block, .menu-item.portfolio').on('click', function () {
@@ -82,9 +85,9 @@ $('.menu-block, .menu-item').click(function () {
         $('.menu-item').removeClass('active');
         $('.menu-item.blog').addClass('active');
     });
-
+    
  
-
+    
     //On Click Close Blocks
     $('#close').on('click', function () {
         $('.name-block').removeClass('reverse');
@@ -101,29 +104,29 @@ $('.menu-block, .menu-item').click(function () {
         $('.inline-menu-container').addClass('show');
         $('.content-blocks.pop section').empty();
     });
-
+    
     $('.menu-block, .menu-item, #close').on('click', function () {
         $('.content-blocks').animate({
             scrollTop: 0
         }, 800);
     });
-
+    
     // Intialize Map
     google.maps.event.addDomListener(window, 'load', init);
-
+    
     function init() {
         // Basic options for a simple Google Map
         // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
         var mapOptions = {
             // How zoomed in you want the map to start at (always required)
             zoom: 11,
-
+    
             // The latitude and longitude to center the map (always required)
             center: new google.maps.LatLng(40.6700, -73.9400),
-
+    
             scrollwheel: false,
-
-
+    
+    
             // How you would like to style the map.
             // This is where you would paste any style found on Snazzy Maps.
             styles: [{
@@ -147,14 +150,14 @@ $('.menu-block, .menu-item').click(function () {
                 }]
             }]
         };
-
+    
         // Get the HTML DOM element that will contain your map
         // We are using a div with id="map" seen below in the <body>
         var mapElement = document.getElementById('map');
-
+    
         // Create the Google Map using our element and options defined above
         var map = new google.maps.Map(mapElement, mapOptions);
-
+    
         var image = 'images/map-marker.png';
         // Let's also add a marker while we're at it
         var marker = new google.maps.Marker({
@@ -165,7 +168,7 @@ $('.menu-block, .menu-item').click(function () {
             animation: google.maps.Animation.DROP
         });
         marker.addListener('click', toggleBounce);
-
+    
         function toggleBounce() {
             if (marker.getAnimation() !== null) {
                 marker.setAnimation(null);
